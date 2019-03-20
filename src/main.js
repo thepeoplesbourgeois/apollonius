012ed50bf50a257b6f713c012dc4115bcfd30c7a
thepeoplesbourgeois <850082+thepeoplesbourgeois@users.noreply.github.com>
2019-03-20 00:47:18 -0700

    Implement location input

--- src/main.js
+++ src/main.js
@@ -1,25 +1,54 @@
 import pixiedust, {render, html, useState} from 'neverland';
 
-const Counter = pixiedust(() => {
-  const [count, setCount] = useState(0);
-  return html`
-  <button onclick=${() => setCount(count + 1)}>
-    Count: ${count}
-  </button>`;
-});
+// const [metadata, setMetadata] = useState(async () => {
+//   const resp = await fetch(`https://archive.org/metadata/${identifier}`);
+//   const json = await resp.json();
+//   console.log(json);
+//   return json;
+// });
+// async function fetchMetadata(event) {
+//   event.preventDefault();
+//   const requests = await Promise.all([
+//     ,
+//
+//   ]);
+//   const [video, related] = await Promise.all(requests);
+//   setMetadata({video, related})
+// };
 
 const Lookup = pixiedust(() => {
-  const [location, setLocation] = useState("");
-  const [metadata, setMetadata] = useState({});
-  const fetchMetadata = async () => {
-    const retrieved = await fetch(`https://archive.org/metadata/${location}`);
+  const [destination, setDestination] = useState("youtube-yBG10jlo9X0");
+  const [identifier, setIdentifier] = useState(destination);
 
+  async function fetchMetadata() {
+    setIdentifier(destination);
+    const vr = fetch(`https://archive.org/metadata/${identifier}`);
+    const rr = fetch(`https://be-api.us.archive.org/mds/v1/get_related/all/${identifier}`);
+    const videoReq = await vr;
+    const relatedReq = await rr;
+    const data = await Promise.all([videoReq.json(), relatedReq.json()]);
+    console.log(data);
+    // TODO: Implement the related video selector and the metadata views
   }
-  return html`
-    <form onsubmit=${fetchMetadata}>
-      <input />
-    </form>
-  `;
-})
 
-render(document.body, Counter);
+  return html`
+    <div>
+      <div>
+        <form onsubmit=${(event) => {event.preventDefault(); fetchMetadata()}}>
+          I want to see archive.org/details/<input type="text" placeholder=${identifier} onchange=${({currentTarget: {value}}) => setDestination(value)} />
+        </form>
+      </div>
+      <iframe
+        src=${`https://archive.org/embed/${identifier}`}
+        width="640"
+        height="480"
+        frameborder="0"
+        webkitallowfullscreen="true"
+        mozallowfullscreen="true"
+        allowfullscreen></iframe>
+
+    </div>
+  `;
+});
+
+render(document.body, Lookup);
